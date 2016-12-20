9755eed3b5cb323aca9e2fecda58be2a702980b6
Anakha Palissery <apalissery@gmail.com>
2016-12-20 18:51:56 -0500

    Add a statick page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   get 'twilio/voice'
 
   get 'access/index'
   get '/login', to: 'access#login'
   get '/signup', to: 'users#new'
   get 'logout', to: 'access#logout'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  
   resources :pins
   resources :users
   #match 'home' => 'users#index' , via:[:get, :post]
