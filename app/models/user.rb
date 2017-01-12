bc61919f0b2f930655881be100d35908742326b6
Anakha Palissery <apalissery@gmail.com>
2017-01-11 23:32:51 -0500

    Make basic user model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,27 @@
 class User < ApplicationRecord
+	
 	has_secure_password
 
 	before_save { |user| user.username = username.downcase }
 	# has many relation with pins
 	has_many :pins, :dependent => :destroy
 
-	validates :username, presence: true
+	validates :name, presence: true, length: { maximum: 50 }
 	validates_uniqueness_of :username, :case_sensitive => false
+
+	before_save { self.email = email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	valides :email, presence: true, length: { maximum: 255 },
+		format: { with: VALID_EMAIL_REGEX }, 
+		uniqueness: { case_sensitive: false }
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 6 }
+
 
 	validates :password, presence: true,
 			  length: {minimum: 1}
 
   	validates :password_confirmation, presence: true
-
-	validates :email, presence: true
 
 	accepts_nested_attributes_for :pins
 end
